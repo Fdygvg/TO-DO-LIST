f5d789cfa211252e00d67ef9af828aad07c576e5
Esowa Chukwuyem <esowaknowledge@email.com>
2025-11-06 18:59:49 -1100

    Added Local-Storage

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const taskContainer = document.getElementById("task-container");
 const inputTask = document.getElementById("input-task");
 
 addTask.addEventListener("click", function () {
+  let i = 0;
+  while (localStorage.getItem(`newTask${i}`) !== null) {
+    ++i;
+  }
+  let taskKey = `newTask${i}`;
   let task = document.createElement("div");
   task.classList.add("task");
-
+  
   let li = document.createElement("li");
   li.innerText = `${inputTask.value}`;
+  
+  localStorage.setItem(taskKey, inputTask.value); //Here
+  task.dataset.key = taskKey;
   task.appendChild(li);
 
   let checkButton = document.createElement("button");
@@ -23,6 +31,8 @@ addTask.addEventListener("click", function () {
 
   if (inputTask.value === "") {
     alert("Please Input A Task");
+    localStorage.removeItem(taskKey);
+
   } else {
     taskContainer.appendChild(task);
   }
@@ -31,10 +41,47 @@ addTask.addEventListener("click", function () {
   checkButton.addEventListener("click", function () {
     checkButton.parentElement.style.textDecoration = " line-through";
     checkButton.parentElement.style.color = "red";
-
   });
   deleteButton.addEventListener("click", function (e) {
     let target = e.target;
-target.parentElement.parentElement.remove()
+    localStorage.removeItem(taskKey);
+    target.parentElement.parentElement.remove();
   });
 });
+
+function loadTasks() {
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key.startsWith("newTask")) {
+      const taskValue = localStorage.getItem(key);
+      let task = document.createElement("div");
+      task.classList.add("task");
+
+      let li = document.createElement("li");
+      li.innerText = taskValue;
+      task.appendChild(li);
+
+      let checkButton = document.createElement("button");
+      checkButton.innerHTML = `<i class="fa-solid fa-check"></i>`;
+      checkButton.classList.add("checkTask");
+      task.appendChild(checkButton);
+
+      let deleteButton = document.createElement("button");
+      deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+      deleteButton.classList.add("deleteTask");
+      task.appendChild(deleteButton);
+
+      taskContainer.appendChild(task);
+
+      checkButton.addEventListener("click", function () {
+        task.style.textDecoration = "line-through";
+        task.style.color = "red";
+      });
+      deleteButton.addEventListener("click", function () {
+        localStorage.removeItem(key);
+        task.remove();
+      });
+    }
+  }
+}
+loadTasks();
